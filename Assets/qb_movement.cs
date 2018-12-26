8724c276de19739f770da54f32f32d11d24c83c9
Goose <goosemixes@gmail.com>
2018-12-25 21:05:36 -0500

    Initial commit

---
+++ Assets/qb_movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class qb_movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float forwardForce = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(transform.up * forwardForce);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(-transform.up * forwardForce);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(transform.right * forwardForce);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-transform.right * forwardForce);
+        }
+    }
+}
